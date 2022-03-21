eb2ae8fa8c5078e93c4c97bc7f60015920b508b2
Leon Matta <le.oni.jfa@gmail.com>
2022-03-21 16:32:01 -0300

    ver0.12

--- Exe08.java
+++ Exe08.java
@@ -1,27 +1,31 @@
 import java.util.Arrays;
 import java.util.Random;
 
-public class Array {
+public class Exe08 {
 
     public static void main(String[] args) {
-        int[] RandomValues = new int[10];
+        int[] valoresRandomicos = new int[10];
         Random random = new Random();
         //o valor acima é para uma array de 1000 elementos, com um fator randomico.
     
-        System.out.println(" Random Values Sorted inside an array: \n");
+        System.out.println(" -- EXERCÍCIO 08 -- \n");
 
         //este >for< funciona para adicionar valores randomicos até 10000 dentro do array.
-        for(int i = 0; i < RandomValues.length; i++){
-            RandomValues[i] = random.nextInt(10000);
+        for(int i = 0; i < valoresRandomicos.length; i++){
+            valoresRandomicos[i] = random.nextInt(10000);
         }
 
+        //for(int i = 0; i < valoresRandomicos.length; i++){
+            //System.out.print(valoresRandomicos[i] + " ");
+
         //esta linha sorteia os arrays em ordem crescente.
-        Arrays.sort(RandomValues);
-        
+        Arrays.sort(valoresRandomicos);
+           
+        //}
 
         //esta linha imprime o array na ordem crescente:
         System.out.println("Elements of array sorted in ascending order : "
-        + Arrays.toString(RandomValues)+"\n");
+        + Arrays.toString(valoresRandomicos)+"\n");
         
     }
 
