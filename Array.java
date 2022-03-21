e10396c56fe292d83a728af85d3680c480deefbe
Leon Matta <le.oni.jfa@gmail.com>
2022-03-21 19:31:33 -0300

    ver0.13

--- Array.java
+++ Array.java
@@ -1,28 +1,88 @@
 import java.util.Arrays;
 import java.util.Random;
+import java.util.Collections;
 
 public class Array {
 
     public static void main(String[] args) {
-        int[] RandomValues = new int[10];
+
+        Integer [] RandomValues = new Integer[5];
         Random random = new Random();
         //o valor acima é para uma array de 1000 elementos, com um fator randomico.
-    
-        System.out.println(" Random Values Sorted inside an array: \n");
 
-        //este >for< funciona para adicionar valores randomicos até 10000 dentro do array.
+        int temp;
+        //variavel para alocar valor temporario para comparação na ordenação que será usado na ordenação manual
+
+        System.out.println("\n >>Random Values Sorted inside an array: <<\n");
+
+        
         for(int i = 0; i < RandomValues.length; i++){
+            
+            //adicionando valores randomicos ao array
             RandomValues[i] = random.nextInt(10000);
+
         }
 
-        //esta linha sorteia os arrays em ordem crescente.
+        //comandos para ordenação automática
         Arrays.sort(RandomValues);
-        
 
-        //esta linha imprime o array na ordem crescente:
-        System.out.println("Elements of array sorted in ascending order : "
-        + Arrays.toString(RandomValues)+"\n");
+        System.out.println("\n Elements of an Array sorted in ascending order AUTOMATICALLY: "+ Arrays.toString(RandomValues)+"\n");
+
+        Arrays.sort(RandomValues, Collections.reverseOrder());
+                
+        System.out.println("\n Elements of an Array sorted in descending order AUTOMATICALLY:  "+ Arrays.toString(RandomValues) + "\n");
+
+        //ordenando manualmente os valores dentro de uma variavel temporaria para ordenação:
+        for(int i = 0; i < RandomValues.length; i++){
+
+            for(int j = i; j < RandomValues.length; j++){
+
+                if(RandomValues[i] > RandomValues[j]){
+
+                    temp = RandomValues[i];
+
+                    RandomValues[i] = RandomValues[j];
+                    RandomValues[j] = temp;
+                    
+
+                }
+            }
+        }
+   
+        //mostrando elementos ordenados manualmente: 
+       System.out.print("\n Elements of the array sorted MANUALLY in ascending order: ");
+        for(int a : RandomValues){
+            System.out.print(" " + a);
+            
+        }
+
+        System.out.println("\n");
         
+        //ordenando de maior para menor manualmente
+        for(int i = 0; i < RandomValues.length; i++){
+
+            for(int j = i; j < RandomValues.length; j++){
+    
+                if(RandomValues[i] < RandomValues[j]){
+    
+                        temp = RandomValues[i];
+    
+                        RandomValues[i] = RandomValues[j];
+                        RandomValues[j] = temp;
+                        
+    
+                }
+               }
+        }
+
+        // //mostrando elementos ordenados manualmente em ordem reversa: 
+        System.out.print("\n Elements of the array sorted MANUALLY in reverse order: ");
+        for(int reverso : RandomValues){
+            System.out.print(" " + reverso);
+        }
+
+        System.out.println("\n");
+           
     }
-
+      
 }
